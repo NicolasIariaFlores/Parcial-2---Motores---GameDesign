3cb4f09fc062b38ef998a36cabdf40923ed9b500
Pipee444 <flpguzman01@gmail.com>
2025-06-11 20:35:13 -0300

    Menu de Pausa

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseButton;
+    [SerializeField] private GameObject _menuPause;
+
+    private bool _gamePaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_gamePaused)
+            {
+                Renude();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        _gamePaused = true;
+        Time.timeScale = 0f;
+        _pauseButton.SetActive(false);
+        _menuPause.SetActive(true);
+    }
+
+    public void Renude()
+    {
+        _gamePaused = false;
+        Time.timeScale = 1f;
+        _pauseButton.SetActive(true);
+        _menuPause.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Cerrando... jiji");
+        Application.Quit();
+    }
+}
