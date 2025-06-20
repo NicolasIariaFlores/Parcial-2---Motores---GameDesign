a899ab28d3c833791465dca213f333804d9dc660
Pipee444 <flpguzman01@gmail.com>
2025-06-19 23:12:51 -0300

    Pantalla de carga en scenas

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,32 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private Slider _loadBar;
+    [SerializeField] private GameObject _loadPanel;
     public void LoadSceneByNumber(int sceneNumber)
     {
-        SceneManager.LoadScene(sceneNumber); 
+        _loadPanel.SetActive(true);
+        StartCoroutine(LoadAsync(sceneNumber));
+    }
+
+    IEnumerator LoadAsync(int sceneNumber)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneNumber);
+        Time.timeScale = 1f;
+        while (!asyncOperation.isDone)
+        {
+            Debug.Log(asyncOperation.progress);
+            _loadBar.value = asyncOperation.progress / 0.9f;
+            yield return null;
+        }
+    }
+    public void Salir()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
     }
 }
